45f2609fc2cb27d9f0ccd2c86c7a7138c32e4e5f
Manish <sahmanish20@gmail.com>
2023-04-29 19:33:35 +0545

    generate random dice

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,19 +1,24 @@
+import React from "react"
 import Die from "./Die"
 
 export default function App() {
+  const [dice, setDice] = React.useState(getAllNewDice())
+
+  function getAllNewDice(){
+    const newDice = []
+    for (let i=0; i<10; i++){
+      newDice.push(Math.ceil(Math.random() * 6))
+
+    }
+    return newDice
+  }
+
+  const diceElements = dice.map(die => <Die value={die}/>)
+
   return (
     <main className="main">
       <div className="dice">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {diceElements}
       </div>
     </main>
   )
