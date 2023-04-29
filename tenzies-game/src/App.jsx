3856bd6df56e50d5fe683a24662c90c23390528f
Manish <sahmanish20@gmail.com>
2023-04-29 19:49:32 +0545

    allow die to be held/unheld

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Die from "./Die"
+import {nanoid} from "nanoid"
 
 export default function App() {
   const [dice, setDice] = React.useState(getAllNewDice())
@@ -7,13 +8,28 @@ export default function App() {
   function getAllNewDice(){
     const newDice = []
     for (let i=0; i<10; i++){
-      newDice.push(Math.ceil(Math.random() * 6))
+      newDice.push(createDie())
 
     }
     return newDice
   }
 
-  const diceElements = dice.map(die => <Die value={die}/>)
+  function createDie(){
+    return {value: Math.ceil(Math.random() * 6), isHeld: false, id: nanoid()}
+  }
+
+  function holdDice(id){
+    console.log("clicked......")
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ?
+      {...die, isHeld: !die.isHeld}:
+      die
+    }))
+
+  }
+
+  const diceElements = dice.map(die => <Die value={die.value} isHeld={die.isHeld} holdDice={() => {holdDice(die.id)}} key={die.id}/>)
+
 
   return (
     <main className="main">
